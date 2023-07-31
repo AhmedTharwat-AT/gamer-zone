89af332e2235a2419216d4e3c3c74debbb773ab3
iMorphex <ahmedsarwat3000@yahoo.com>
2023-07-31 17:58:42 +0300

    added .env file with api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function App() {
+  console.log(process.env.REACT_APP_ApiKey);
+  const Apikey = process.env.REACT_APP_ApiKey;
   return (
     <div className="App">
       <header className="App-header">
         <p>hello from desktop</p>
+        <p>{process.env.REACT_APP_ApiKey}</p>
       </header>
     </div>
   );
