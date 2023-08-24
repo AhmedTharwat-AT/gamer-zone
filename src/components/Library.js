5868d58b5cb0a6db5fecc8d75005318919eaf900
iMorphex <ahmedsarwat3000@yahoo.com>
2023-08-24 14:12:28 +0300

    added library styles (need more work)

--- src/components/Library.js
+++ src/components/Library.js
@@ -5,18 +5,25 @@ export default function Library({ onClickGame, libraryGames }) {
   return (
     <div className="library-container">
       {libraryGames?.length > 0 ? (
-        <div className="library-games">
-          <GamesList games={libraryGames} onClickGame={onClickGame} />
-        </div>
+        <>
+          <h1 className="library-title">Library Games</h1>
+          <div className="library-games">
+            <GamesList games={libraryGames} onClickGame={onClickGame} />
+          </div>
+        </>
       ) : (
         <div className="library-empty">
-          Image by{" "}
-          <a href="https://www.freepik.com/free-vector/cardboard-boxes-collection-shipment_2005023.htm#page=4&query=empty%20box&position=33&from_view=search&track=ais">
-            Freepik
-          </a>
-          <h1>Library is Empty</h1>
-          <img src="/images/box.png"></img>
-          <p>Add game to you library</p>
+          <h1 className="library-title">Library is Empty</h1>
+          <div>
+            <img src="/images/box.png"></img>
+            <div>
+              Image by{" "}
+              <a href="https://www.freepik.com/free-vector/cardboard-boxes-collection-shipment_2005023.htm#page=4&query=empty%20box&position=33&from_view=search&track=ais">
+                Freepik
+              </a>
+            </div>
+          </div>
+          <p>Add games to your library</p>
         </div>
       )}
     </div>
